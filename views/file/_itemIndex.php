b901863fddd2914c0c8801ced00136a9cd2ae6e6
Satrio Arditama <iyo@amilna.com>
2015-03-24 10:11:23 +0700

    file admin

---
+++ views/file/_itemIndex.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+
+$module = Yii::$app->getModule('blog');
+?>
+
+	<div class="thumbnail">	
+	
+		<div >										
+			<div class="caption text-left"  style="margin:0px 0px 0px 0px;background:#ffffff;padding:20px;">
+				<h3><?= Html::a($model->title,$model->file,["target"=>"blank"]) ?></h3>
+				<h5><small><?= Html::encode(date('D d M, Y',strtotime($model->time))) ?></small></h5>									
+			
+				<p class=""><?= Html::encode($model->description) ?></p>
+				<p>
+				<?= Html::a(Yii::t('app','Download'),$model->file,['class'=>'btn btn-small btn-default',"target"=>"blank"]) ?>			
+				</p>
+			</div>		
+		</div>
+	</div>
+
