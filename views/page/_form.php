cbf3c650d841c617edba96f1d932c103c592ed00
Satrio Arditama <iyo@amilna.com>
2015-06-01 13:58:02 +0700

    forms

--- views/page/_form.php
+++ views/page/_form.php
@@ -127,7 +127,7 @@ if ($module->enableUpload)
 					'data'=>$model->getTags(),
 					'pluginOptions' => [
 						'tags' => true,
-						'tokenSeparators'=>[',',' '],
+						'tokenSeparators'=>[','],
 					],
 				]) ?>										
 				
